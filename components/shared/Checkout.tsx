65e14bc988d5525b0e1c630eede90959199eefbd
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-12-18 21:32:54 +0300

    .

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
-import { loadStripe } from '@stripe/stripe-js';
+// import { loadStripe } from '@stripe/stripe-js';
 
 import { IEvent } from '@/lib/database/models/event.model';
 import { Button } from '../ui/button';
 import { checkoutOrder } from '@/lib/actions/order.actions';
 
-loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+// loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
   useEffect(() => {
