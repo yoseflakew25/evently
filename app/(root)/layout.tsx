acac6a06f130ff38460520febf92045a2b13c8a3
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-12-18 19:45:01 +0300

    .

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-// import Footer from "@/components/shared/Footer";
-// import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
 
 export default function RootLayout({
   children,
@@ -8,9 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <div className="flex h-screen flex-col">
-      {/* <Header /> */}
+      <Header />
       <main className="flex-1">{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
